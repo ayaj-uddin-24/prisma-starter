5556f8dc7f0a1df670130cafc8642829181b03a4
ayaj-uddin-24 <ayajuddin2024@gmail.com>
2025-09-24 16:27:27 +0600

    Refactor user creation code to comment out and retrieve user by ID

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import { PrismaClient } from "../generated/prisma/index.js";
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.user.create({
-    data: {
-      name: "Tanif Uddin",
-      email: "tanifuddin@gmail.com",
-      profile: "https://ayazishere.vercel.app/ayaz.jpz",
+  // const result = await prisma.user.create({
+  //   data: {
+  //     name: "Tanif Uddin",
+  //     email: "tanifuddin@gmail.com",
+  //     profile: "https://ayazishere.vercel.app/ayaz.jpz",
+  //   },
+  // });
+
+  // console.log(result);
+
+  const result = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: 4,
     },
   });
 
